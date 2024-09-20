c905f59c2a2d63bb63819e00ac5b674917de4114
Ayushman Tomar <141900459+AyushmanTomar@users.noreply.github.com>
2024-09-21 03:24:57 +0530

    added full front end

--- app.py
+++ app.py
@@ -9,11 +9,17 @@ def home():
 
 @app.route('/login')
 def signin():
-    return render_template('login.html')
+    return render_template('login.html',error = None)
 
 @app.route('/recall')
 def retrieve():
-    return render_template('recall.html')
+    return render_template('recall.html',response=None)
+
+@app.route('/update')
+def update():
+    return render_template('update.html',error = None)
+
+#768
 
 if __name__ == '__main__':
     app.run(debug=True)
