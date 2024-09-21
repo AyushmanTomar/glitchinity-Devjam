71c00248c50cb9810a25a16ce2ffd8a3fcf46b57
Atharva Goliwar <atharva.goliwar23@gmail.com>
2024-09-22 03:11:23 +0530

    ok

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask,render_template,request,jsonify
-from auth import auth_bp
+from auth import auth_bp,jwt_is_required,getCookieInfo
 import memorai
 from memorai import ask_model
 from memorai import upload_experience
-from imageai import get_caption
-import os
-
 app = Flask(__name__)
 app.register_blueprint(auth_bp)
 
@@ -19,46 +16,32 @@ def signin():
     return render_template('login.html',error = None)
 
 @app.route('/recall')
+@jwt_is_required
 def retrieve():
     return render_template('recall.html',response=None)
 
 @app.route('/recallmemory',methods=['POST'])
+@jwt_is_required
 def recall_():
+    token = request.cookies.get('cookie')
+    user_info = getCookieInfo(token)
     query = request.form['memory']
-    memory= ask_model(query)
+    memory= ask_model(query,user_info[0]['sub'])
     return jsonify({'memory': memory})
 
 # for updating the data base with new memory
 @app.route('/update')
+@jwt_is_required
 def update():
     return render_template('update.html',error = None)
 
 @app.route('/updatememory',methods=['POST'])
 def update_():
-    file = request.files['file']
-    if file:
-        print("yes bro file present!!")
-        if file.filename == '':
-            return jsonify({"memory": "File is corrupted"})
-        filepath = os.path.join("uploads", file.filename)
-        file.save(filepath)
-        caption = get_caption(filepath)
-        caption = upload_experience(caption)
-        return jsonify({"memory": caption})
-    else:
-        exp = request.form['memory']
-        exp= upload_experience(exp)
-        return jsonify({'memory': exp})
-
-
-@app.route("/uploadimg", methods=[ "POST"])
-def image_():
-    if request.method == "POST":
-        if 'file' not in request.files:
-            return "No file uploaded", 400
-        
-    return render_template("index.html")
-
+    exp = request.form['memory']
+    token = request.cookies.get('cookie')
+    user_info = getCookieInfo(token)
+    exp= upload_experience(exp,user_info['sub'])
+    return jsonify({'memory': exp})
 
 
 
